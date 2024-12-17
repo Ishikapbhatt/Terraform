1e26f46631f16399ac0ae0f6f1d062149f4a716d
Ishikapbhatt <ishikap1998@gmail.com>
2024-12-17 18:20:53 +0530

    Create main.tf

---
+++ destroy-t/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-example-bucket"
+}
