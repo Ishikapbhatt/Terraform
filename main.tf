dd975a7c2ecea6c0abc59d7a0970f91db95f7d0d
Ishikapbhatt <ishikap1998@gmail.com>
2024-12-17 14:35:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,11 +10,11 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-053b12d3152c0cc71"
   instance_type = "t2.micro"
 
   tags = {
